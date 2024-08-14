a4dc888e5faa8cb626ecddecfa3c7c32c3d805eb
Lidia Bylicka <lidiabylicka@gmail.com>
2024-08-14 20:08:34 +0100

    feat/ register works

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,29 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "ChefsKissApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "chefskiss9ae43a5b": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
+      "HostedUIDomain": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "LambdaFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- layouts/LoggedIn.tsx
+++ layouts/LoggedIn.tsx
@@ -1,22 +1,40 @@
+"use client";
+
+import { Amplify } from "aws-amplify";
+
+// Imports the Authenticator and withAuthenticator components from '@aws-amplify/ui-react'.
+// Authenticator is a React component that provides a ready-to-use sign-in and sign-up UI.
+// withAuthenticator is a higher-order component that wraps your app component to enforce authentication.
+import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import { useForm } from "react-hook-form";
+
+// Imports the default styles for the Amplify UI components. This line ensures that the authenticator looks nice out of the box.
+import "@aws-amplify/ui-react/styles.css";
+import awsConfig from "@/aws-exports";
+
 import Navbar from "./Navbar";
 
 type LoggedInProps = {
   children: React.ReactNode;
 };
+Amplify.configure(awsConfig);
 
 const LoggedIn: React.FC<LoggedInProps> = ({ children }) => {
+  console.log("useForm", useForm);
   return (
     <div className="w-full h-full gap-20 flex-col justify-center flex">
-      <Navbar>
-        <div>Left hand side</div>
-        <div></div>
-        <div>Right Links</div>
-      </Navbar>
-      <div className="flex flex-col p-12">
-        <p>{children}</p>
-      </div>
+      <Authenticator>
+        <Navbar>
+          <div>Left hand side</div>
+          <div></div>
+          <div>Right Links</div>
+        </Navbar>
+        <div className="flex flex-col p-12">
+          <div>{children}</div>
+        </div>
+      </Authenticator>
     </div>
   );
 };
 
-export default LoggedIn;
+export default withAuthenticator(LoggedIn);
